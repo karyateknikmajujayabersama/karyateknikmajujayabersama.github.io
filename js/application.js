338ac4a9fac2789818b2bc881b0e5ac613cc9750
ricky.chandra <ricky.chandra@grabtaxi.com>
2020-11-30 00:59:43 +0800

    feat: add company profile and add carousel

--- js/application.js
+++ js/application.js
@@ -1,2 +1,34 @@
+(function() {
+	"use strict";
+
+	$(".carousel").on("click", function(e) {
+		let $el = $(e.currentTarget);
+		let $parent = $el.closest(".styles");
+		let allImages = $parent.children(".image-container");
+
+		let shownImage = $parent.children(".image-container").not(".hidden").first();
+		let shownImageId = +shownImage.attr("data-item-id");
+
+		let nextImageId;
+		if ($el.hasClass("left")) {
+			nextImageId = shownImageId - 1;
+			if (nextImageId <= 0) {
+				nextImageId = allImages.length;
+			}
+		} else {
+			nextImageId = shownImageId + 1;
+			if (nextImageId > allImages.length) {
+				nextImageId = 1;
+			}
+		}
+
+		let nextImage = $parent.children(".image-container[data-item-id=" + nextImageId + "]").first();
+
+		shownImage.addClass("hidden");
+		nextImage.removeClass("hidden");
+	});
+})();
+
+
 // Not used.
 // 
